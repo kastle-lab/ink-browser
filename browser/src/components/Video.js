7271a466822d1dd7ccd652ea0b31b2b0b49d340d
Michael McCain <mccain.32@wright.edu>
2024-09-24 07:57:40 -0400

    Video component now includes URL bar

--- browser/src/components/Video.js
+++ browser/src/components/Video.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Video() {
 
+  // URL input State 
+  const [url, setUrl] = useState('');
+
+  // Default URL
+  const [videoUrl, setVideoUrl] = useState('https://www.youtube.com/embed/y7sXDpffzQQ?si=fLAVlojPQW0P6CE-')
+
+  // Udate URL during typing
+  const handleChange = (event) => {setUrl(event.target.value)};
+
+  // Handle key presses
+  const handleKeyPress = (event) => {
+    if (event.key == 'Enter') {
+      const embedUrl = convertUrl(String(url));
+      setVideoUrl(embedUrl);
+      event.preventDefault();
+    }
+  }
+
+  // Function takes in default YouTube URL and converts it to an embedded video
+  function convertUrl(url) {
+    if (String(url).includes('watch?v=')) {
+      return url.replace(/watch\?v=/, 'embed/')
+    }
+    return url 
+  }
+
   return (
     <div> 
+      <input 
+        type="text"
+        value = {url}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder='Enter YouTube URL here and press Enter'
+        style = {{width:'100%', padding: '10px'}}
+      />
       <p align="center">
       <iframe
         width="640"
         height="480"
-        src="https://www.youtube.com/embed/y7sXDpffzQQ?si=fLAVlojPQW0P6CE-"
+        src={videoUrl}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
